fd14ff4f107b8ebb258a81e9222523d687d91776
minaemad13 <menaemadorai@gmail.com>
2022-02-19 13:16:43 +0200

    edit some lines in models

--- users/models.py
+++ users/models.py
@@ -12,9 +12,7 @@ from django.db import models
 
 
 class User(models.Model):
-    fullname = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    Email = models.EmailField(max_length=240)
+   pass
 
 
 class Cars(models.Model):

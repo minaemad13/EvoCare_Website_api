5f42897a907d6db02ddc21daa2c76e55b5828216
John <51532708+JohnM28@users.noreply.github.com>
2022-03-01 05:03:38 +0200

    added Qr Code Generator (#8)

--- users/models.py
+++ users/models.py
@@ -4,8 +4,13 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from pyexpat import model
-
-
+import qrcode
+import random
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
+from django.db.models.signals import pre_delete,post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -42,3 +47,44 @@ class Appointments(models.Model):
     User_Id = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
 
 # >>>>>>> 7ba510865f8bb9413ccf27aa48965404b7c63837
+
+
+class Qr(models.Model):
+    user_id = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    vip = models.BooleanField(default=False)
+    qr_code = models.ImageField(upload_to='qr_codes', blank=True)
+
+    class Meta:
+        verbose_name = ('QR code')
+
+    def __str__(self):
+        return str(self.user_id.First_Name)
+
+    def create_qr(self):
+        if self.vip:
+            print(self.user_id.id)
+            url = f"http://127.0.0.1:8000/admin/users/userprofile/{self.user_id.id}"
+            qrcode_img = qrcode.make(
+                url)
+            canvas = Image.new('RGB', (450,400), 'white')
+            canvas.paste(qrcode_img)
+            fname = f'qr_code-{self.user_id}{random.randint(1000, 9999)}.png'
+            buffer = BytesIO()
+            canvas.save(buffer, 'PNG')
+            self.qr_code.save(fname, File(buffer), save=False)
+            canvas.close()
+
+    def save(self, *args, **kwargs):
+        self.create_qr()
+        super(Qr, self).save(*args, **kwargs)
+
+
+
+@receiver(pre_delete, sender=Qr)
+def qr_delete(sender, instance, **kwargs):
+    instance.qr_code.delete(False)
+
+# @receiver(post_save, sender=Qr)
+# def update_history_field(sender, instance, **kwargs):
+#  if instance.vip == False:
+#     instance.qr_code.delete(False)

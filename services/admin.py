be4a4713733bf8a15602b0ed2c5257120136138b
John <51532708+JohnM28@users.noreply.github.com>
2022-03-04 05:33:13 +0200

    Admin custom John (#10)
    * added custom admin panel

--- services/admin.py
+++ services/admin.py
@@ -5,16 +5,22 @@ from . import models
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('sv_id', 'title', 'alt', 'image', 'date_uploaded', 'status')
     prepopulated_fields = {'image': ('sv_id',), }
+    icon_name = 'image'
 
 @admin.register(models.ServicesVideos)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('sv_id', 'title', 'alt', 'video', 'date_uploaded', 'status')
     prepopulated_fields = {'video': ('sv_id',), }
-
+    icon_name = 'movie'
 
 @admin.register(models.Packages)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('sv_id', 'pck_name', 'pck_price', 'status')
     prepopulated_fields = {'pck_name': ('sv_id',), }
+    icon_name = 'add_shopping_cart'
 
-admin.site.register(models.Services)
+# admin.site.register(models.Services)
+
+@admin.register(models.Services)
+class AuthorAdmin(admin.ModelAdmin):
+    icon_name = 'local_car_wash'
